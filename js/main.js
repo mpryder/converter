90869f4ee5f9a5c02bdb50273733d96f677960fa
Mark Ryder <ryddlemethis@gmail.com>
2014-03-06 19:34:09 +0000

    heroku commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window._skel_config={
+    prefix: 'style',
+        preloadStyleSheets: true,
+    resetCSS: true,
+    boxModel: 'border',
+    grid: { gutters: 30 },
+    breakpoints: {
+        wide: { range: '1100-', containers: 1140, grid: { gutters: 60 } },
+        narrow: { range: '481-1099', containers: 960 },
+        mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true } }
+    }
+}
