fea335d8d655b56ff0c68c9ee8f593d0b11f69c0
Mark Ryder <ryddlemethis@gmail.com>
2014-03-07 18:26:22 +0000

    sixth commit

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ window._skel_config={
     boxModel: 'border',
     grid: { gutters: 30 },
     breakpoints: {
-        wide: { range: '1100-', containers: 1140, grid: { gutters: 60 } },
-        narrow: { range: '481-1099', containers: 960 },
+        wide: { range: '1200-', containers: 1140, grid: { gutters: 50 } },
+        narrow: { range: '481-1199', containers: 960 },
         mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true } }
     }
 }
